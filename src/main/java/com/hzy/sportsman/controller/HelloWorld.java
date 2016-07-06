b6692aa8bed4b5be26548b05fdbda4eb57e463ae
hzy1992 <584512169@qq.com>
2016-07-07 02:53:01 +0800

    hello world

---
+++ src/main/java/com/hzy/sportsman/controller/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.hzy.sportsman.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns="hello")
+public class HelloWorld extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+		arg1.getWriter().write("hello world");
+	}
+
+	
+}
